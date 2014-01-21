cd6f3643874e38171ed03bda71b098b780fe3ed1
Paul LaMotte <pulsaral@gmail.com>
2014-01-21 03:05:25 -0600

    added networking

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string typeName = "2Pac";
+	private const string gameName = "2Pac Test Room";
+	
+	private void StartServer()
+	{
+		Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+
+	void OnServerInitialized()
+	{
+		SpawnPlayer();
+	}
+
+	void OnGUI()
+	{
+		if (!Network.isClient && !Network.isServer)
+		{
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				StartServer();
+			
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+				RefreshHostList();
+			
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+						JoinServer(hostList[i]);
+				}
+			}
+		}
+	}
+
+	private HostData[] hostList;
+	
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log("Server Joined");
+		SpawnPlayer();
+	}
+
+	void OnPlayerDisconnected(NetworkPlayer player)
+	{
+		Network.RemoveRPCs(player);
+		Network.DestroyPlayerObjects(player);
+	} 
+	
+	public GameObject playerPrefab;
+
+	
+	private void SpawnPlayer()
+	{
+		Vector3 pos = new Vector3(0,0,0);
+		switch (Network.connections.Length) {
+		case 0: pos.x = 1.5f; pos.y = 1.5f; break;
+		case 1: pos.x = 17.5f; pos.y = 1.5f; break;
+		case 2: pos.x = 1.5f; pos.y = 8.5f; break;
+		case 3: pos.x = 17.5f; pos.y = 8.5f; break;
+		}
+
+		GameObject player = (GameObject) Network.Instantiate(playerPrefab, pos, Quaternion.identity, 0);
+
+
+		PacmanAnimate animate = (PacmanAnimate) player.GetComponent("PacmanAnimate");
+		animate.maxSpeed = 0.049f;
+		animate.direction.x = 0;
+		player.transform.position = pos;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/PacmanAnimate.cs
+++ Assets/PacmanAnimate.cs
@@ -12,44 +12,45 @@ public class PacmanAnimate : MonoBehaviour {
 	}
 	
 	void Update () {
-		Vector3 startPos = transform.position;
-		Vector2 newDirection = new Vector2( 0, 0 );
+		if (networkView.isMine) {
+			Vector3 startPos = transform.position;
+			Vector2 newDirection = new Vector2( 0, 0 );
 
-		// compute the attempted direction
-		if ( Input.GetKey( KeyCode.LeftArrow ) ) newDirection.x -= 1;
-		else if ( Input.GetKey( KeyCode.RightArrow ) ) newDirection.x += 1;
-		else if ( Input.GetKey( KeyCode.DownArrow ) ) newDirection.y -= 1;
-		else if ( Input.GetKey( KeyCode.UpArrow ) ) newDirection.y += 1;
+			// compute the attempted direction
+			if ( Input.GetKey( KeyCode.LeftArrow ) ) newDirection.x -= 1;
+			else if ( Input.GetKey( KeyCode.RightArrow ) ) newDirection.x += 1;
+			else if ( Input.GetKey( KeyCode.DownArrow ) ) newDirection.y -= 1;
+			else if ( Input.GetKey( KeyCode.UpArrow ) ) newDirection.y += 1;
 
-		if ( newDirection.x + newDirection.y != 0 )
-		{
-			// decide if attempted direction is a valid one based on whether or not it would effect mr
-			// pacman were it executed
-			newDirection.Normalize( ); 
-			newDirection.Scale( new Vector2( maxSpeed, maxSpeed ) );
-			Vector3 posAfter = OnStart.board.tryMove( transform.position, newDirection );
-
-			if ( Vector3.Distance( posAfter, startPos ) >= .001 )
+			if ( newDirection.x + newDirection.y != 0 )
 			{
-				// valid velocity change.
-				direction = newDirection;
+				// decide if attempted direction is a valid one based on whether or not it would effect mr
+				// pacman were it executed
+				newDirection.Normalize( ); 
+				newDirection.Scale( new Vector2( maxSpeed, maxSpeed ) );
+				Vector3 posAfter = OnStart.board.tryMove( transform.position, newDirection );
+
+				if ( Vector3.Distance( posAfter, startPos ) >= .001 )
+				{
+					// valid velocity change.
+					direction = newDirection;
+				}
+			}
+
+			direction.Normalize( );
+			direction *= maxSpeed;
+
+			transform.position = OnStart.board.tryMove( transform.position, direction );
+
+			// rotate to face direction traveling 
+			bool[] point = new bool[]{direction.x > 0, direction.y > 0, direction.x < 0, direction.y < 0 };
+			for ( int i = 0; i < 4; i++ )
+			{
+				if ( point[i] )
+				{
+					transform.rotation = Quaternion.Euler( new Vector3(0, 0, i * 90 ));
+				}
 			}
 		}
-
-		direction.Normalize( );
-		direction *= maxSpeed;
-
-		transform.position = OnStart.board.tryMove( transform.position, direction );
-
-		// rotate to face direction traveling 
-		bool[] point = new bool[]{direction.x > 0, direction.y > 0, direction.x < 0, direction.y < 0 };
-		for ( int i = 0; i < 4; i++ )
-		{
-			if ( point[i] )
-			{
-				transform.rotation = Quaternion.Euler( new Vector3(0, 0, i * 90 ));
-			}
-		}
-
 	}
 }
