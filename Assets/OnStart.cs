b8c9a4f91d27a266038bfcf5d909e69bcef9e1f9
MadDogMcNamara <rjmcnama@mtu.edu>
2014-01-21 00:33:56 -0600

    added board movement logic

---
+++ Assets/OnStart.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class OnStart : MonoBehaviour {
+
+	public GameObject filledCell;
+	public static GameBoard board;
+
+	public void createCell( int x, int y, bool fill )
+	{
+		if ( fill ) 
+		{
+			GameObject cell = (GameObject) Instantiate( filledCell );
+			cell.transform.position = new Vector3(x + .5f,y + .5f, 0f);
+		}
+	}
+
+	void Start () {
+		// todo, this would  be read from a file or something
+		string[] sboard = new string[] { 
+			"1111111111111111111",
+			"1000000001000000001",
+			"1011011101011101101",
+			"1000000000000000001",
+			"1011010111110101101",
+			"1000010001000100001",
+			"1111011101011101111",
+			"1111010000000101111",
+			"1000000111110000001",
+			"1111111111111111111" };
+
+		bool[,] board = new bool[sboard.Length,sboard[0].Length];
+		for ( int i = 0; i < board.GetLength(0); i++ )
+		{
+			for (int j = 0; j < board.GetLength(1); j++ )
+			{
+				board[i,j] = sboard[i].ToCharArray()[j] == '1';
+				createCell( j, i, board[i,j] );
+			}
+		}
+		OnStart.board = new GameBoard( board );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/PacmanAnimate.cs
+++ Assets/PacmanAnimate.cs
@@ -1,45 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using AssemblyCSharp;
 
 public class PacmanAnimate : MonoBehaviour {
 
 
-	public float maxSpeed = .1f;
+	public float maxSpeed = .01f;
+	public Vector2 direction = new Vector2( -1, 0 );
 
-	// Use this for initialization
 	void Start () {
-	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		if ( Input.GetKey( KeyCode.LeftArrow ) )
+		Vector3 startPos = transform.position;
+		Vector2 newDirection = new Vector2( 0, 0 );
+		
+		if ( Input.GetKey( KeyCode.LeftArrow ) ) newDirection.x -= 1;
+		else if ( Input.GetKey( KeyCode.RightArrow ) ) newDirection.x += 1;
+		else if ( Input.GetKey( KeyCode.DownArrow ) ) newDirection.y -= 1;
+		else if ( Input.GetKey( KeyCode.UpArrow ) ) newDirection.y += 1;
+
+		if ( newDirection.x + newDirection.y != 0 )
 		{
-			Vector3 newpos = transform.position;
-			newpos.x -= maxSpeed;
-			transform.position = newpos;
-			transform.localRotation = Quaternion.Euler( 0, 0, 180 );
+			newDirection.Normalize( ); 
+			float asdfasdf = newDirection.x * maxSpeed;
+			newDirection.Scale( new Vector2( maxSpeed, maxSpeed ) );
+			Vector3 posAfter = OnStart.board.tryMove( transform.position, newDirection );
+
+			if ( Vector3.Distance( posAfter, startPos ) >= .001 )
+			{
+				// valid velocity change.\
+				direction = newDirection;
+			}
 		}
-		if ( Input.GetKey( KeyCode.RightArrow ) )
+
+		direction.Normalize( );
+		direction.Scale( new Vector2( maxSpeed, maxSpeed ) );
+
+		transform.position = OnStart.board.tryMove( transform.position, direction );
+
+		bool[] point = new bool[]{direction.x > 0, direction.y > 0, direction.x < 0, direction.y < 0 };
+		for ( int i = 0; i < 4; i++ )
 		{
-			Vector3 newpos = transform.position;
-			newpos.x += maxSpeed;
-			transform.position = newpos;
-			transform.localRotation = Quaternion.Euler( 0, 0, 0 );
-		}
-		if ( Input.GetKey( KeyCode.UpArrow ) )
-		{
-			Vector3 newpos = transform.position;
-			newpos.y += maxSpeed;
-			transform.position = newpos;
-			transform.localRotation = Quaternion.Euler( 0, 0, 90 );
-		}
-		if ( Input.GetKey( KeyCode.DownArrow ) )
-		{
-			Vector3 newpos = transform.position;
-			newpos.y -= maxSpeed;
-			transform.position = newpos;
-			transform.localRotation = Quaternion.Euler( 0, 0, 270 );
+			if ( point[i] )
+			{
+				transform.rotation = Quaternion.Euler( new Vector3(0, 0, i * 90 ));
+			}
 		}
 
 	}
