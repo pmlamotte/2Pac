60fa57ba71f45b036ed18b5b1491cc924875edd5
Paul LaMotte <pulsaral@gmail.com>
2014-01-26 15:59:35 -0600

    Centered main menu

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -3,25 +3,35 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
+	private const int WIDTH = 400;
+	public const int BUTTON_HEIGHT = 50;
+	public GUIStyle style;
+
 	// Use this for initialization
 	void Start () {
 		PlayerInfo playerInfo = PlayerInfo.Instance;
 	}
 
+	private int centeredXPos() {
+		return Screen.width / 2 - WIDTH / 2;
+	}
+
 	void OnGUI() {
 
-		if (GUI.Button(new Rect(100, 100, 250, 100), "Single Player")) {
+		GUI.Label(new Rect(centeredXPos(), 100, WIDTH, 75), new GUIContent("2PAC"), style);
+
+		if (GUI.Button(new Rect(centeredXPos(), 225, WIDTH, BUTTON_HEIGHT), "Single Player")) {
 			GetComponent<SinglePlayer>().enabled = true;
 			this.enabled = false;
 		}
 
-		if (GUI.Button(new Rect(100, 300, 250, 100), "Online Multiplayer")) {
+		if (GUI.Button(new Rect(centeredXPos(), 325, WIDTH, BUTTON_HEIGHT), "Online Multiplayer")) {
 			GetComponent<MultiplayerLobby>().enabled = true;
 			this.enabled = false;
 		}
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
