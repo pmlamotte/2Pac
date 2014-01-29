fa9495b1bda728d97a335094a4764a6e4bebf218
Paul LaMotte <pulsaral@gmail.com>
2014-01-28 18:59:13 -0600

    removed debug statements

--- Assets/Scripts/Board/Warpable.cs
+++ Assets/Scripts/Board/Warpable.cs
@@ -23,9 +23,7 @@ public class Warpable : MonoBehaviour {
 		BoardLocation location = obj.boardLocation;
 		IntVector2 pos = location.location;
 		foreach (Warp warp in data.WarpPoints.Values) {
-			Debug.Log("Pos: " + pos.x + "," + pos.y + "  Warp: " + warp.input.x + "," + warp.input.y);
 			if (pos.Equals (warp.input)) {
-				Debug.Log("wat");
 				obj.boardLocation = new BoardLocation(warp.output.Clone(), warp.outOffset.Clone());
 				break;
 			}
